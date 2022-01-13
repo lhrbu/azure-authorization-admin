b68b7154c46c58309bf70391de721fda55246463
lhrbuxiaoxin <lihaoran228@163.com>
2022-01-13 21:21:13 +0800

    add role assign page

--- src/Pages/UsersPage.tsx
+++ src/Pages/UsersPage.tsx
@@ -4,11 +4,16 @@ import SensitiveData from '../Models/SensitiveData';
 import nameof from '../Utils/nameof';
 import md5 from 'md5'
 import { UsersAdminWebAPI, UsersWebAPI } from '../WebAPIs/UsersWebAPI';
+import Modal from 'antd/lib/modal/Modal';
+import AssignRoleEditor from '../Components/AssignRoleEditor';
+import { CloseOutlined } from '@ant-design/icons';
+import { RolesAdminWebAPI } from '../WebAPIs/RolesWebAPI';
 
 export default function UsersPage()
 {
     const [uploading, setUploading] = useState<boolean>(false)
     const [userIds,setUserIds] = useState<string[]>([])
+    const [modalVisible,setModalVisible] = useState<boolean>(false)
 
     useEffect(()=>{
         FetchData()
@@ -17,7 +22,7 @@ export default function UsersPage()
     return (
         <Fragment>
             <div className='Mauve page-title'>Users Admin</div>
-            <Form
+            <Form autoComplete='off' autoSave='off'
                 labelCol={{ span: 8 }}
                 wrapperCol={{ span: 8 }}
                 onFinish={async values =>
@@ -52,21 +57,21 @@ export default function UsersPage()
                     name={nameof<SensitiveData>("UserId")}
                     rules={[{ required: true }]}
                 >
-                    <Input autoComplete='off'/>
+                    <Input/>
                 </Form.Item>
                 <Form.Item 
                     label="Password"
                     name="Password"
                     rules={[{ required: true }]}
                 >
-                    <Input.Password autoComplete='off'/>
+                    <Input.Password/>
                 </Form.Item>
                 <Form.Item
                     label="Confirm Password"
                     name="ConfirmPassword"
                     rules={[{ required: true }]}
                 >
-                    <Input.Password autoComplete='off'/>
+                    <Input.Password/>
                 </Form.Item>
                 <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
                     <Button type="primary" htmlType="submit" loading={uploading}>
@@ -74,12 +79,31 @@ export default function UsersPage()
                     </Button>
                 </Form.Item>
             </Form>
-            <hr style={{margin:"48px 16px"}}/>
+            <hr style={{margin:"32px 0px"}}/>
+            <div style={{marginBottom:"32px"}}>
+                <Button type="primary" onClick={()=>setModalVisible(true)}>Assign Role</Button>
+                <Modal visible={modalVisible}  footer={null} onCancel={()=>setModalVisible(false)}>
+                    <AssignRoleEditor/>
+                </Modal>
+            </div>
+            
             <Row gutter={[16, 16]}>
                 {
                     userIds.map(userId=>(
                         <Col span={8} key={userId}>
-                            <Card>
+                            <Card extra={(
+                                 <CloseOutlined onClick={async ()=>{
+                                    if(window.confirm("delete user?"))
+                                    {
+                                        try{
+                                            setUploading(true)
+                                            await UsersAdminWebAPI.Delete(userId);
+                                            message.success("Delete successfully",2)
+                                        }catch(err){message.error((err as any).message,2)}
+                                        finally{setUploading(false)}
+                                    }
+                                 }}/>
+                            )}>
                                 <Card.Grid style={{ width: "50%", textAlign: "left" }}>
                                     {userId}
                                 </Card.Grid>
